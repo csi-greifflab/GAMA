92c8e75e9dc6b2a1912d7fe2bd90f55e13b92c7a
Robert Frank <rofrank2@gmail.com>
2022-12-11 16:25:22 +0100

    ig_array.sh fixed slurm script

--- ig_array.sh
+++ ig_array.sh
@@ -3,23 +3,27 @@
 #SBATCH --account=nn9603k
 #SBATCH --job-name=ig_array
 
-#SBATCH --array=0-16
+#SBATCH --array=1-2
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=1
+#SBATCH --gpus=1
 #SBATCH --partition=accel
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=4G
+#SBATCH --ntasks=1
 
-#  5min
-#SBATCH --time=0-00:05:00
+##  7d
+#SBATCH --time=7-00:00:00
 
 ## Set safer defaults for bash
 set -o errexit
 set -o nounset
 
-module --quiet purge  # Clear any inherited modules
-module load conda!!!!!!!!!!!!!!!!!!!
-conda activate ig
+
+## module --quiet purge  # Clear any inherited modules
+## module load Anaconda3/2020.11
+## module list
+## conda activate ig
 
 # each job will see a different ${SLURM_ARRAY_TASK_ID}
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
-python3 singleGPU.py ${SLURM_ARRAY_TASK_ID}
+## python3 testbla.py ${SLURM_ARRAY_TASK_ID} ./test
+python3 singleGPU.py ${SLURM_ARRAY_TASK_ID} ./ig_mass
